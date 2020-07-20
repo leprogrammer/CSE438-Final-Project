1fb6e2d69a1ef3a96b396a630831ca81c3194693
Tejas Prasad <tejas.prasad07@gmail.com>
2020-07-20 18:29:30 -0400

    Initial Commit

---
+++ CSE438_FinalProject/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CSE438_FinalProject
+//
+//  Created by Tejas Prasad on 7/20/20.
+//  Copyright © 2020 Tejas Prasad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSE438_FinalProject/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CSE438_FinalProject
+//
+//  Created by Tejas Prasad on 7/20/20.
+//  Copyright © 2020 Tejas Prasad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
