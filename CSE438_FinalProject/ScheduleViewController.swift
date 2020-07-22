417d336f0d1074545b288f5c88d1a429c514506e
Tejas Prasad <tejas.prasad07@gmail.com>
2020-07-21 21:40:44 -0400

    Added all view controllers

---
+++ CSE438_FinalProject/ScheduleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleViewController.swift
+//  CSE438_FinalProject
+//
+//  Created by Tejas Prasad on 7/21/20.
+//  Copyright © 2020 Tejas Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CSE438_FinalProject/SearchResultsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultsViewController.swift
+//  CSE438_FinalProject
+//
+//  Created by Tejas Prasad on 7/21/20.
+//  Copyright © 2020 Tejas Prasad. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
